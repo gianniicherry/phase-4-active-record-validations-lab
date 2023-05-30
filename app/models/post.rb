f3412f7411275138284672f1712ca25968008e6d
Gianni <gianniicherry@gmail.com>
2023-05-29 21:04:33 -0700

    commit tree 234572daffdf2e7885371dc0aeab3c52701abe0c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :sufficiently_clickbait_y
+
+  private
+
+  def sufficiently_clickbait_y
+    unless title.present? &&
+           (title.include?("Won't Believe") ||
+            title.include?("Secret") ||
+            title.match?(/\bTop \d+\b/) ||
+            title.include?("Guess"))
+    end
+end
 end
